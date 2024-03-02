5e2dbc4ad38c1fe6525b24c60c538a9e78bd60df
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-03-02 10:35:51 +0530

    write some utils files

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,7 +14,7 @@ connectDB()
         throw error
     })
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
