d985ec16b652517328d2fc0e0309b429e798a854
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-03-02 12:44:17 +0530

    add user model , blog model and add middleware

---
+++ Backend/src/models/blog.model.js
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from "mongoose";
+
+const blogSchema = new Schema(
+    {
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    thumbnail:{
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+},
+{
+    timestamps: true
+}
+);
+
+export const Blog = mongoose.model('Blog', blogSchema);
